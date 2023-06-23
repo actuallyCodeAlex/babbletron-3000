ee610b9f948b02fd11cf9d9c76a4d8023d73c6e0
actuallyCodeAlex <actuallyCodeAlex@gmail.com>
2023-06-23 13:49:56 -0700

    Update App

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,26 @@
 import { useEffect, useState } from "react";
 import * as ReactDOM from "react-dom/client";
+import { RepoDisplay } from "./components/repo-display";
+import { RepoList } from "./components/repo-list";
 
 const App = () => {
-  const [projects, setProjects] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+  const [content, setContent] = useState(null);
 
   useEffect(() => {
-    // 1. Make PROJECTS REQ to MIDDLEWARE
-    const localProjectServerUrl = "http://localhost:8000/projects";
-    fetch(localProjectServerUrl).then((res) => res.json())
+    // 1. Make REPOS REQ to MIDDLEWARE
+    const repos = "http://localhost:8000/repos";
+    fetch(repos).then((res) => res.json())
       .then((response) => {
         console.log(response);
-        setProjects(response.repos);
+        setRepositories(response.repos);
       }).catch((error) => console.error(error))
   }, [])
   return (
     <>
-      <h1>Sanity + Electron + React</h1>
-      <main>
-        <h2>Projects</h2>
-        <h3>FROM SANITY</h3>
-        <ul>
-          <li>Loading projects...</li>
-        </ul>
-        <div>
-          <pre>
-            ¯\_(ツ)_/¯ Your data will show up here when you've configured
-            everything correctly
-          </pre>
-        </div>
-        <h3>FROM MIDDLEWARE</h3>
-        <ol>
-          {projects.map((project) => <li key={project.id}>{project.name}</li>)}
-        </ol>
-      </main>
+      <h1>babbletron-3000</h1>
+      <RepoList repositories={repositories} setContent={setContent} />
+      <RepoDisplay content={content} />
     </>
   );
 };
