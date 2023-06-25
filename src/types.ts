594c742a6e115edfc0fcb2bf3c4238e1d4146901
actuallyCodeAlex <actuallyCodeAlex@gmail.com>
2023-06-25 13:17:56 -0700

    Lint w prettier

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,24 @@
+export enum NavOptions {
+  Home = "Home",
+  Repositories = "Repositories",
+  Settings = "Settings",
+}
+
 export enum OwnerType {
-    User = "User"
+  User = "User",
 }
 
 export type Owner = {
-    id: number;
-    login: string;
-    type: OwnerType
-}
+  id: number;
+  login: string;
+  type: OwnerType;
+};
 
 export type Repository = {
-    description: null | string;
-    id: number;
-    name: string;
-    owner: Owner;
-    private: boolean;
-    url: string;
-}
+  description: null | string;
+  id: number;
+  name: string;
+  owner: Owner;
+  private: boolean;
+  url: string;
+};
